2d370bf4e0e326a051bba742534b6cd117536cc9
wuxd251688 <36547863@qq.com>
2016-07-25 23:04:55 +0800

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,5 +33,6 @@ Sample1App::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  ActiveModel::SecurePassword.min_cost = true
 
 end
